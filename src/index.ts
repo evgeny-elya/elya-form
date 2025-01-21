e31a735d1e708184b7d8dce0da4fee257994ce0e
Evgeny Elyashevich <evgeny.elyashevich@solbeg.com>
2025-01-22 00:09:01 +0300

    init project

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import './index.scss';
+
+const message: string = 'Hello, T2ype23sssScript w2ith Webpack!';
+console.log(message);
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development', // Режим разработки или продакшен
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    devServer: {
+        static: './dist',
+        port: 3000,
+        hot: true,
+        open: true,
+        watchFiles: ['src/**/*.html'],
+    },
+};
